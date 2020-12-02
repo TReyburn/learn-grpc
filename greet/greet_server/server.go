871cb3bbfcc9a7ab12fce3d37c2c869da283b6e8
Travis Reyburn <treyburn@alteryx.com>
2020-12-01 17:41:47 -0700

    added new line for more clear printing

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -12,7 +12,7 @@ import (
 type server struct{}
 
 func (s *server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
-	fmt.Printf("Greet call invoked: %v", req)
+	fmt.Printf("Greet call invoked: %v\n", req)
 	fn := req.GetGreeting().GetFirstName()
 	result := "Hello " + fn
 	res := greetpb.GreetResponse{Result: result}
