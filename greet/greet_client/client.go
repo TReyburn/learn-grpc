18f293c46d7d0c42ccaa96ba1438caad32482aaf
Travis Reyburn <treyburn@alteryx.com>
2020-12-01 17:12:26 -0700

    added client boilerplate

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"../greetpb"
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+)
+
 func main() {
-	$END$
+	fmt.Println("hello im a client")
+
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+	defer cc.Close()
+
+	c := greetpb.NewGreetServiceClient(cc)
+	fmt.Println("created client", c)
 }
