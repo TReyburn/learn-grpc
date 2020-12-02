85a16cb6ce8bb79f4a7de197a640ce607a8c8646
Travis Reyburn <treyburn@alteryx.com>
2020-12-02 12:36:18 -0700

    implemented client streaming endpoint on server

--- calculator/calculator_server/server.go
+++ calculator/calculator_server/server.go
@@ -3,26 +3,57 @@ package main
 import (
 	"../calculatorpb"
 	"context"
+	"errors"
 	"fmt"
 	"google.golang.org/grpc"
+	"io"
 	"log"
 	"net"
 )
 
 type server struct{}
 
+func (s server) Average(stream calculatorpb.CalculatorService_AverageServer) error {
+	nums := make([]int32, 0)
+
+	for {
+		req, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error while streaming: %v", err)
+		}
+		nums = append(nums, req.GetNumber())
+	}
+
+	res := float32(0.0)
+
+	for _, num := range nums {
+		res += float32(num)
+	}
+
+	res /= float32(len(nums))
+
+	err := stream.SendAndClose(&calculatorpb.AverageResponse{Result: res})
+	if err != nil {
+		log.Fatalf("Error when sending response: %v", err)
+	}
+
+	return nil
+}
+
 func (s server) PrimeNumberDecomposition(
 	req *calculatorpb.PrimeNumberRequest,
 	stream calculatorpb.CalculatorService_PrimeNumberDecompositionServer,
 ) error {
-
 	fmt.Println("Prime Number Decomposition Service Invoked")
 	n := req.GetNumber()
 	primeNumberDecomposition(n, stream)
 	return nil
 }
 
-func (s server) Sum(ctx context.Context, req *calculatorpb.SumRequest) (*calculatorpb.SumResponse, error) {
+func (s server) Sum(_ context.Context, req *calculatorpb.SumRequest) (*calculatorpb.SumResponse, error) {
 	fmt.Println("Sum Service Invoked")
 	fn := req.GetFirstNum()
 	sn := req.GetSecondNum()
@@ -54,7 +85,7 @@ func primeNumberDecomposition(n int32, steam calculatorpb.CalculatorService_Prim
 			if err != nil {
 				log.Fatalf("Error while attempting to send stream: %v", err)
 			}
-			n = n / k
+			n /= k
 		} else {
 			k++
 		}
